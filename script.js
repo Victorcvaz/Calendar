4a2413feefb7ddfdf8022ebfff5765a60dd14b47
Victorcvaz <102685506+Victorcvaz@users.noreply.github.com>
2022-04-13 16:30:07 -0300

    :package: data e hora

---
+++ script.js
@@ -0,0 +1,26 @@
+function displayTime() {
+    let allTimes = new Date();
+    let hAtual = allTimes.getHours()
+    let mAtual = allTimes.getMinutes()
+    let sAtual = allTimes.getSeconds()
+    let diasemAtual = allTimes.toLocaleDateString('pt-BR', {weekday: 'long'})
+    let diaAtual = allTimes.getDate()
+    let mesAtual = allTimes.toLocaleDateString('pt-BR', {month: 'long'})
+    let anoAtual = allTimes.getFullYear()
+    if (mAtual < 10) {
+         mAtual="0"+mAtual;
+    } if (sAtual < 10) {
+         sAtual="0"+mAtual   
+    } else {
+        document.querySelector('.clock').innerHTML = `${hAtual}:${mAtual}:${sAtual}`
+    }
+    
+        document.querySelector(".date").innerHTML = `${diasemAtual}, ${diaAtual} de ${mesAtual} de ${anoAtual}`
+
+    setTimeout(function () {
+        displayTime()
+    }, 1000)
+}
+
+
+
